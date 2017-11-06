3e4ff95154b4e53e99ee1dd99397150ebdef09de
Santino Valenzuela <s0nnyv123@gmail.com>
2017-11-06 02:02:43 -0500

    move js files

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 // Points all the files that exists in the /src folder
 var SOURCEPATHS = {
     sassSource : 'src/scss/*.scss', // Any file with .scss extension
-    htmlSource : 'src/*.html' // Listens to all html files inside the /src folder
+    htmlSource : 'src/*.html', // Listens to all html files inside the /src folder
+    jsSource : 'src/js/**/*.js' // Listens to any JavaScript files in the /js folder.
 }
 
 // Points all the files that exists in the /app folder
@@ -17,6 +19,17 @@ var APPPATH = {
     js : 'app/js'
 }
 
+// This task is to remove file(s) from /app folder when its corresponding file is removed from the /src folder.
+gulp.task('clean-html', function(){
+    return gulp.src(APPPATH.root + '/*.html', {read: false, force: true}) // This is to look for html files and not read the contents.
+        .pipe(clean());
+});
+
+gulp.task('clean-scripts', function(){
+    return gulp.src(APPPATH.js + '/*.js', {read: false, force: true}) // This is to look for html files and not read the contents.
+        .pipe(clean());
+});
+
 gulp.task('sass', function(){
     return gulp.src(SOURCEPATHS.sassSource)
         .pipe(autoprefixer())
@@ -24,8 +37,13 @@ gulp.task('sass', function(){
         .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('scripts', ['clean-scripts'], function() {
+    gulp.src(SOURCEPATHS.jsSource)
+        .pipe(gulp.dest(APPPATH.js))
+});
+
 // Copies html file from the /src folder to /app folder
-gulp.task('copy', function(){
+gulp.task('copy', ['clean-html'], function(){
     gulp.src(SOURCEPATHS.htmlSource)
         .pipe(gulp.dest(APPPATH.root))
 });
@@ -39,9 +57,10 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy'], function(){
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts'], function(){
     gulp.watch([SOURCEPATHS.sassSource], ['sass']);
-    gulp.watch([SOURCEPATHS.htmlSource], ['copy'])
+    gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+    gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
 });
 
 gulp.task('default', ['watch']);
