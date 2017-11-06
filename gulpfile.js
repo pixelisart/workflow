43db60a5be821b6445318fdb35ae78c15f20b34b
Santino Valenzuela <s0nnyv123@gmail.com>
2017-11-06 00:20:41 -0500

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// Points all the files that exists in the /src folder
+var SOURCEPATHS = {
+    sassSource : 'src/scss/*.scss' // Any file with .scss extension
+}
+
+// Points all the files that exists in the /app folder
+var APPPATH = {
+    root: 'app/',
+    css : 'app/css',
+    js : 'app/js'
+}
 
 gulp.task('sass', function(){
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(SOURCEPATHS.sassSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function(){
+    // browserSync will initialize all css, js and html when browserSync is initialized.
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+        server: {
+            baseDir : APPPATH.root
+        }
+    })
+});
+
+gulp.task('watch', ['serve', 'sass'], function(){
+    gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);
